2c079e3bc1d211089c9876db984e5d8fcdb9b9e6
manuelillolg <manulopez18899@gmail.com>
2023-08-30 13:11:08 +0200

    End of section 8: POKEMON api with CRUD operations and SEED generator

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from '@nestjs/common';
 import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { CreatePokemonDto } from 'src/pokemon/dto/create-pokemon.dto';
 
 
 @Injectable()
 export class SeedService {
   private readonly axios : AxiosInstance = axios;
+
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>
+  ){}
  
  async  executeSeed(){
-  const {data} =  await this.axios.get<PokeResponse>('http://pokeapi.co/api/v2/pokemon?limit=10');
-  data.results.forEach(({name,url})=>{
+  await this.pokemonModel.deleteMany({});
+  const {data} =  await this.axios.get<PokeResponse>('http://pokeapi.co/api/v2/pokemon?limit=20');
+
+  const pokemonToInsert: {name:string,no:number}[] =[];
+
+  data.results.forEach(async({name,url})=>{
     const segments = url.split('/');
     const no:number = +segments[segments.length -2];
+
+    pokemonToInsert.push({name,no});
+
   })
 
+  await this.pokemonModel.insertMany(pokemonToInsert);
+  
   
   return data.results;
  }
